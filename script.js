225ad98307354286075b25e02d2fa7a3b13894a3
Vidhi jain <117736922+vi3003@users.noreply.github.com>
2023-03-26 23:25:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const arr = [1, 2, 3, 4];
+function promise(arr, time) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(arr);
+    }, time);
+  });
+}
+const op = document.getElementById("output");
+promise(arr, 3000)
+  .then((res) => {
+    const evenArr = res.filter((num) => {
+      return num % 2 === 0;
+    });
+    return promise(evenArr, 1000);
+  })
+  .then((res) => {
+    
+    op.innerText = res;
+    const multiplyBy2 = res.map((num) => {
+      return num * 2;
+    });
+
+    return promise(multiplyBy2, 2000);
+  })
+  .then((res) => {
+    op.innerText = res;
+  });
